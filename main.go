ab63b8a9cb2203b8460a81c6ef9cfdfa3547e19a
raihaninfo <raihanmahmudi35@gmail.com>
2022-01-09 19:18:34 +0600

    test code

--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"fmt"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
 	a := app.New()
+
 	w := a.NewWindow("Mini ERP - Add new Client")
 	w.Resize(fyne.NewSize(500, 500))
 
@@ -17,18 +21,23 @@ func main() {
 	emailEntry := widget.NewEntry()
 	addressEntry := widget.NewMultiLineEntry()
 
-	name := widget.NewFormItem("Name", nameEntry)
-	mobile := widget.NewFormItem("Mobile", mobileEntry)
-	email := widget.NewFormItem("Email", emailEntry)
-	address := widget.NewFormItem("Address", addressEntry)
-	clientForm := widget.NewForm(name, mobile, email, address)
-	
-	clientForm.SubmitText = "Save"
+	name1 := widget.NewFormItem("Name", nameEntry)
+	mobile1 := widget.NewFormItem("Mobile", mobileEntry)
+	email1 := widget.NewFormItem("Email", emailEntry)
+	address1 := widget.NewFormItem("Address", addressEntry)
+	clientForm := widget.NewForm(name1, mobile1, email1, address1)
+
+	clientForm.SubmitText = "Login"
 
 	clientForm.OnSubmit = func() {
+		name := nameEntry.Text
+		mobile := mobileEntry.Text
+		email := emailEntry.Text
+		address := addressEntry.Text
+
+		fmt.Println(name, mobile, email, address)
 
 	}
-	
 
 	w.SetContent(
 		container.NewGridWithColumns(1,

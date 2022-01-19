2191632a00259a48d1035baaf766fb3a8e90cbd1
raihaninfo <raihanmahmudi35@gmail.com>
2022-01-19 11:03:29 +0600

    create test button

--- test/main.go
+++ test/main.go
@@ -4,12 +4,19 @@ package main
 import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
 	a := app.New()
 	w := a.NewWindow("Test Window")
 	w.Resize(fyne.NewSize(600, 600))
+
+	btn := widget.NewButton("test", func() {
+
+	})
+
+	w.SetContent(btn)
 
 	w.ShowAndRun()
 }

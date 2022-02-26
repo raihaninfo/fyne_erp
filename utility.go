e2108d875b50714588bda7cca79c9e38875ff008
raihaninfo <raihanmahmudi35@gmail.com>
2022-02-26 19:29:08 +0600

    folder name change function remove day

--- utility.go
+++ utility.go
@@ -7,11 +7,9 @@ import (
 )
 
 func CreateFolderUseingDate() (string, string) {
-	day := time.Now().Day()
 	month := time.Now().Month().String()
 	year := time.Now().Year()
-
-	folder := fmt.Sprintf("%v-%v-%v", day, month, year)
+	folder := fmt.Sprintf("%v-%v", month, year)
 	fl := fmt.Sprintf("pdf/%s", folder)
 	os.Mkdir(fl, 0755)
 
